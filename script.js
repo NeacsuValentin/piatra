0e44d90579c48ce7b1d642777f994bb57750d307
ValentinNeacsu <neacsu_valentin_iulian@yahoo.ro>
2020-05-12 20:44:48 +0300

    first

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log('Start Game')
+
+var possibilities = ["rock","paper","scissors"];
+
+const user = possibilities[Math.floor(Math.random()*possibilities.length)]
+
+const computer = possibilities[Math.floor(Math.random()*possibilities.length)]
+
+console.log({user,computer});
+
+function getWinner(user,computer) {
+    console.log({user,computer});
+    let winner;
+    if (userChoice ===computerChoise) {
+        winner = 'draw';
+        var test ="2"
+    } else {-
+    }
+    console.log()
+    return winner
+
+}
+
+
+console.log(getWinner(user,computer));
